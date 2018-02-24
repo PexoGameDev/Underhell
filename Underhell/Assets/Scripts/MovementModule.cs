470727d097691deb8386bfd5f81650ec91a13c01
Pexo <contact@pexogamedev.pl>
2018-02-24 10:04:32 +0100

    Basic scaffolding for AI's movement IQ system
    They walk over the map and don't fall to death, although on IQ = 2 they even jump on higher platforms (first they make sure they are capable of jumping up there) and even jump down to other platforms (but not off the map)

--- Underhell/Assets/Scripts/MovementModule.cs
+++ Underhell/Assets/Scripts/MovementModule.cs
@@ -5,14 +5,7 @@ using UnityEngine;
 public class MovementModule : MonoBehaviour {
     #region Variables
     // Fields //
-    private bool isJumping = false;
-    private bool hasDoubleJumped = false;
-    private bool isDashing = false;
-
-    private int groundLayerMask;
-    private int rotation;
-
-    private float actualJumpMaxHeight = Mathf.Infinity;
+    [Range(0, 5)] public int MovementIQ;
 
     [SerializeField] private float maxHorizontalVelocity = 35f;
     [SerializeField] private float maxVerticalVelocity = 20f;
@@ -23,6 +16,16 @@ public class MovementModule : MonoBehaviour {
     [SerializeField] private float dashDistance = 10f;
     [SerializeField] private float dashCooldown = 2f;
 
+    private bool isJumping = false;
+    private bool hasDoubleJumped = false;
+    private bool isDashing = false;
+
+    private int groundLayerMask;
+    private int rotation;
+
+    private float actualJumpMaxHeight = Mathf.Infinity;
+
+    private Rigidbody rb;
     // Public Properties //
     public int Rotation
     {
@@ -70,18 +73,92 @@ public class MovementModule : MonoBehaviour {
     }
 
     void Start () {
-		
+        rb = GetComponent<Rigidbody>();
+        Rotation = 1;
 	}
 	
 	void Update () {
-		
-	}
+        Debug.DrawRay(transform.position - Vector3.up * transform.localScale.y * 0.75f, Vector3.right * Rotation, Color.red);
+        Debug.DrawRay(transform.position + Vector3.up * JumpHeight, Vector3.right * Rotation,Color.green);
+        Debug.DrawRay(transform.position + Vector3.up * JumpHeight / 2, Vector3.right * Rotation, Color.yellow);
+        Debug.DrawRay(transform.position + Vector3.up * JumpHeight / 4, Vector3.right * Rotation, Color.magenta);
+
+        Debug.DrawRay(transform.position + Vector3.right * transform.localScale.x * Rotation, Vector3.down, Color.blue);
+    }
+
+    void FixedUpdate()
+    {
+        switch(MovementIQ)
+        {
+            case 0: break;
+
+            case 1:
+                if (CheckGround())
+                    DoStep();
+                else
+                    Rotation *= -1;
+                break;
+
+            case 2:
+                if (CheckJumpableWall() && !isJumping)
+                    Jump();
+                else
+                    goto case 1;
+                break;
+        }
+
+        if (actualJumpMaxHeight < transform.position.y)
+        {
+            rb.velocity = new Vector3(0, 0, 0);
+            rb.AddForce(Vector3.down * jumpForce / 2, ForceMode.Impulse);
+        }
+        else
+            rb.velocity = new Vector3(0, rb.velocity.y, 0);
+
+    }
     #endregion
 
     #region Public Methods
     #endregion
 
     #region Private Methods
+    private bool CheckGround()
+    {
+        return (Physics.Raycast(transform.position + Vector3.right * transform.localScale.x * Rotation, Vector3.down, transform.localScale.y * 10, groundLayerMask));
+    }
+
+    private bool CheckJumpableWall()
+    {
+        return (Physics.Raycast(transform.position - Vector3.up * transform.localScale.y * 0.75f, Vector3.right * Rotation, transform.localScale.x, groundLayerMask) && !Physics.Raycast(transform.position + Vector3.up*JumpHeight,Vector3.right*Rotation,transform.localScale.x,groundLayerMask));
+    }
+
+    private void Jump()
+    {
+        int JumpStep = 1;
+        if (!Physics.Raycast(transform.position + Vector3.up * JumpHeight / 2, Vector3.right * Rotation, transform.localScale.x, groundLayerMask))
+            JumpStep = 2;
+        if (!Physics.Raycast(transform.position + Vector3.up * JumpHeight / 4, Vector3.right * Rotation, transform.localScale.x, groundLayerMask))
+            JumpStep = 4;
+
+        rb.AddForce(Vector3.up * JumpForce / JumpStep, ForceMode.Impulse);
+        actualJumpMaxHeight = transform.position.y + JumpHeight;
+        isJumping = true;
+        InvokeRepeating("ResetJump", 0.1f, 0.1f);
+    }
+
+    private void DoStep()
+    {
+        transform.position += Vector3.right * Rotation * MovementSpeed;
+    }
+
+    void ResetJump()
+    {
+        if (Physics.Raycast(transform.position, Vector3.down, transform.localScale.y, groundLayerMask))
+        {
+            isJumping = false;
+            CancelInvoke("ResetJump");
+        }
+    }
     #endregion
 }
 
