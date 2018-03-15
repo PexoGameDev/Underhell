6c5010ef82e85d67e1d2a834fc5345d7e84b6336
Mmero <dragonuppl@gmail.com>
2018-03-15 23:24:31 +0100

    little cahnges

---
+++ Underhell/Assets/Graphics/slowdonwbymero.cs
@@ -0,0 +1,16 @@
+﻿
+using UnityEngine;
+
+public class slowdonwbymero : MonoBehaviour {
+
+	public float slowdonwFactor = 0.05f;
+	
+	void Start () 
+	{
+		Time.timeScale = slowdonwFactor;
+		Time.fixedDeltaTime = Time.timeScale * .02f;
+	}
+	
+	
+}
+
