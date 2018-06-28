973dc7acf8a679c4d87346051f1be5d33f986e40
Pexo <contact@pexogamedev.pl>
2018-06-28 17:45:00 +0200

    Added target FPS script

---
+++ Underhell/Assets/Scripts/GameControllers/TargetFPS.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetFPS : MonoBehaviour {
+
+    [SerializeField] private int targetFPS = 120;
+
+    void Awake () {
+        Application.targetFrameRate = targetFPS;
+    }
+}
