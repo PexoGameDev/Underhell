2dba0cac12b1513edd52b1b824d2f7eb32ed9b93
Pexo <contact@pexogamedev.pl>
2018-06-28 17:47:44 +0200

    Fixing flame arc

--- Underhell/Assets/Scripts/Spells/FlameArc.cs
+++ Underhell/Assets/Scripts/Spells/FlameArc.cs
@@ -2,24 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FlameArc : Spell {
+public class FlameArc : ChanneledSpell {
     #region Variables
     // Fields //
     [Header("Flame Arc")]
     [SerializeField] private float arcLenght = 25f;
     [SerializeField] private int DPS = 5;
-
-    private float duration = 0f;
     // Public Properties //
 
     // Private Properties //
-    private float Duration
-    {
-        get { return duration; }
-        set {
-                    
-            }
-    }
     #endregion
 
     #region Unity Methods
@@ -42,28 +33,16 @@ public class FlameArc : Spell {
         arc.SetColors(Color.yellow, Color.red);
 
         RaycastHit hit;
-
-        while (Input.GetMouseButton(1))
+        if(Physics.Raycast(Player.Entity.transform.position,endPoint, out hit, arcLenght, 1 << LayerMask.NameToLayer("Player")))
         {
-            if(Physics.Raycast(Player.Entity.transform.position,endPoint, out hit, arcLenght))
+            arc.SetPositions(new Vector3[] { Player.Entity.transform.position, hit.point});
+            if (hit.collider.gameObject.GetComponent<HPModule>())
             {
-                arc.SetPositions(new Vector3[] { Player.Entity.transform.position, hit.point});
-                if (hit.collider.gameObject.GetComponent<HPModule>())
-                {
-                    hit.collider.gameObject.GetComponent<HPModule>().GetHit(DPS, 0, Vector3.zero, AttackEffect.DamageSource.Fire);
-                }
+                hit.collider.gameObject.GetComponent<HPModule>().GetHit(DPS, 0, Vector3.zero, AttackEffect.DamageSource.Fire);
             }
-            else
-                arc.SetPositions(new Vector3[] { Player.Entity.transform.position, endPoint });
-
-            if ((duration += Time.fixedDeltaTime) >= SpellcastingModule.maxSpellDuration)
-            {
-                duration = 0f;
-                return true;
-            }
-
-            return false;
         }
+        else
+            arc.SetPositions(new Vector3[] { Player.Entity.transform.position, endPoint });
 
         return true;
     }
