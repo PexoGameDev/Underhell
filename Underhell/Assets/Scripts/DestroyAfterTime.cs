e573252ca5e01cf6e1318b8ff39e1c1b738c50b7
Pexo <contact@pexogamedev.pl>
2018-07-01 10:06:05 +0200

    Added DestroyAfterTime script

---
+++ Underhell/Assets/Scripts/DestroyAfterTime.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfterTime : MonoBehaviour {
+
+    public float TimeToDestroy = 1f;
+	void Start () {
+        Destroy(gameObject, TimeToDestroy);
+	}
+}
